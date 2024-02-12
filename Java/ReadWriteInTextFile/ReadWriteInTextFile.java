5b33573ec8a9343bb58799f75aafea1a04084217
khali ait miloud <you@example.com>
2024-02-12 10:32:54 +0100

    update java file

--- Java/ReadWriteInTextFile/ReadWriteInTextFile.java
+++ Java/ReadWriteInTextFile/ReadWriteInTextFile.java
@@ -1,33 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.readwriteintextfile;
 import java.io.*;
-/**
- *
- * @author lenovo
- */
+
 public class ReadWriteInTextFile {
+    static void CreateFile(String fileName){
+      try {
+        File file = new File(fileName);
+        if (file.createNewFile()) {
+               System.out.println("File created: " + file.getName());
+        } else {
+            System.out.println("File already exists.");
+        }
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+      }
+    }
     
-    static void ReadFile(String filePath) {
-        char[] array = new char[100];
-        System.out.println("Reading " + filePath + "...");
+    static void WriteFile(String fileName,String text){
         try {
-            FileReader reader = new FileReader(filePath);
+            FileWriter file = new FileWriter(fileName);
+            file.write(text);
+            file.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+    
+    static void ReadFile(String fileName) {
+        char[] array = new char[50];
+        System.out.println("Reading " + fileName + "...");
+        try {
+            FileReader reader = new FileReader(fileName);
             BufferedReader BufferedReader = new BufferedReader(reader);
             // Reads characters
             BufferedReader.read(array);
             System.out.println("Data in the file: ");
-            System.out.println(array);
+            System.out.println(array+"...");
             BufferedReader.close();
         } catch (Exception e) {
         }
     }
+    
+    static void DeleteFile(String fileName){
+        File file = new File(fileName); 
+        if (file.delete()) { 
+            System.out.println("Deleted the file: " + file.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        } 
+    }
     public static void main(String[] args) {
-        String filePath = "Text.txt";
-        ReadFile(filePath);
-
-        System.out.println("Hello World!");
+        
+        String fileName = "Text.txt";
+        String text = "This is a line of text inside the file.";
+        
+        CreateFile(fileName);
+        WriteFile(fileName,text);
+        ReadFile(fileName);
+        //DeleteFile(fileName);
     }
 }
