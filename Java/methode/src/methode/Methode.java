72713fbd19320b7a39ee68c3519aa4e6b80e5f47
khalid ait miloud <aitmiloudkhaled@gmail.com>
2024-05-06 14:00:51 +0100

    drs ta3 class

---
+++ Java/methode/src/methode/Methode.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package methode;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Methode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Etudiant etu1 = new Etudiant("over-k");
+        System.out.println(etu1.toString());
+    }
+    
+}
