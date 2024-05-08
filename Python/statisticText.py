31144040fef8c85e04190ca3e9f89e025748d02f
khalid ait miloud <aitmiloudkhaled@gmail.com>
2024-05-08 14:58:01 +0100

    add python file to statictic text

---
+++ Python/statisticText.py
@@ -0,0 +1,11 @@
+file = open("Python/text.txt", "r").read().strip().replace("\n"," ").replace("."," ").replace(","," ")
+words = file.split(" ")
+characters = file.split(" ")
+staticsWords = {}
+for word in words:
+    try:
+        staticsWords[word] += 1
+    except:
+        staticsWords[word] = 0
+print(staticsWords)
+    
