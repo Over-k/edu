98751a9fce4e3befbdba785b8c238f89a6b45e70
khalid ait miloud <aitmiloudkhaled@gmail.com>
2024-05-11 15:38:46 +0100

    complete statisticText ex

--- Python/statisticText.py
+++ Python/statisticText.py
@@ -1,11 +1,36 @@
-file = open("Python/text.txt", "r").read().strip().replace("\n"," ").replace("."," ").replace(","," ")
+fileName = "Python/text.txt"
+
+# words
+file = open(fileName, "r").read().lower().strip().replace("\n"," ").replace("."," ").replace(","," ")
 words = file.split(" ")
-characters = file.split(" ")
 staticsWords = {}
 for word in words:
-    try:
-        staticsWords[word] += 1
-    except:
-        staticsWords[word] = 0
-print(staticsWords)
-    
+    if "" != word:
+        try:
+            staticsWords[word] += 1
+        except:
+            staticsWords[word] = 1
+sortedWords = sorted(staticsWords.items(),key=lambda x:x[1],reverse=True)
+
+# characters
+characters = file
+staticsCharacters = {}
+for character in characters:
+    if character.isalnum():
+        try:
+            staticsCharacters[character] += 1
+        except:
+            staticsCharacters[character] = 1
+sortedCharacters = sorted(staticsCharacters.items(),key=lambda x:x[1],reverse=True)
+
+def tablePrint(title,myData):
+    print(title)
+    max_col1 = max(len(col1) for col1, _ in myData)
+    max_col2 = len(str(max(col2 for _, col2 in myData)))
+    print("="*(7+max_col1+max_col2))
+    for row1, row2 in myData:
+        print(f"| {row1:{max_col1}} | {row2:{max_col2}} |")
+    print("_"*(7+max_col1+max_col2))
+
+tablePrint("\nData of characters :", sortedCharacters)
+tablePrint("\nData of words :", sortedWords)
